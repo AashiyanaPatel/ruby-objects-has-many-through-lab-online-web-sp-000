270150c030800307c08fb16a7afccaaccfe5fbfa
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-10 01:31:07 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,3 +6,12 @@ class Patient
     @name = name
     @appointments = []
   end
+  
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
+  end
+  
+  def appointments
+    @appointments
+  end
