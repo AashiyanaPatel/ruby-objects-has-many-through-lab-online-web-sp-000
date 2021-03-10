ae629b12e39bb3ecf503e4af8fa49a09dcc7ea01
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-10 01:48:28 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,17 +12,19 @@ class Doctor
    def self.all
     @@all
   end
+  
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
 
   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
 
   def patients
-    appointments.map {|appointment| appointment.patient}
+    appointments.map(&:patient)
   end
 
-  def new_appointment(date, patient)
-    Appointment.new(date, patient, self)
-  end
+  
 
 end
