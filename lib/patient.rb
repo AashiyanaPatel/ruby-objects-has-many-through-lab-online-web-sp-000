14bdc7d6b0e6d1857e8350c2378acadddc46c29a
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-10 01:05:40 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
 
   def doctors
-    appointments.all.collect {|appointment| appointment.doctor}
+    appointments.collect {|appointment| appointment.doctor}
   end
 
   def new_appointment(doctor, date)
